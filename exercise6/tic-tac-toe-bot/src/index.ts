9f6a994b1b7605ae00e96dd9a1189494d044923d
Annurkenova <gann3115@gmail.com>
2023-11-23 18:20:10 +0600

    changes in index ts

--- exercise6/tic-tac-toe-bot/src/index.ts
+++ exercise6/tic-tac-toe-bot/src/index.ts
@@ -1,49 +1,45 @@
+// index.ts
 import "./index.css";
-import {controller, signal} from "./utils/abortSignal.ts";
+import { controller, signal } from "./utils/abortSignal.ts";
 import App from "./app.ts";
-import SlytherinUser from "./model/SlytherinUser.ts";
-import GryffindorUser from "./model/GryffindorUser.ts";
-import RavenclawUser from "./model/RavenclawUser.ts";
-import HufflepuffUser from "./model/HufflepuffUser.ts";
+import SlytherinBot from "./model/SlytherinBot.ts";
+import SlytherinUser from "./model/SlytherinUser.ts"
 
-window.addEventListener("unload", unloadHandler, {once: true});
-window.addEventListener("load", loadHandler, {once: true});
+window.addEventListener("unload", unloadHandler, { once: true });
+window.addEventListener("load", loadHandler, { once: true });
 
 function unloadHandler() {
-    // NOTE: no need for removeEventListener;
-    controller.abort();
+  controller.abort();
 }
 
 async function loadHandler() {
-    const app = getApp();
+  const app = getApp();
 
-    if (!app) {
-        return;
-    }
+  if (!app) {
+    return;
+  }
 
-    window.addEventListener("pushstate", app.render, {signal});
-    window.addEventListener("popstate", app.render, {signal});
+  window.addEventListener("pushstate", app.render, { signal });
+  window.addEventListener("popstate", app.render, { signal });
 
-    await app.render();
+  await app.render();
 }
 
 function getApp() {
-    const appEl = document.getElementById("app");
+  const appEl = document.getElementById("app");
 
-    if (!appEl) {
-        return null;
-    }
+  if (!appEl) {
+    return null;
+  }
 
-    const players = getPlayers();
+  const players = getPlayers();
 
-    return new App(appEl, players);
+  return new App(appEl, players);
 }
 
 function getPlayers() {
-    const gryffindorUser = new GryffindorUser();
-    const slytherinUser = new SlytherinUser();
-    const ravenclawUser = new RavenclawUser();
-    const hufflepuffUser = new HufflepuffUser();
+  const slytherinBot = new SlytherinBot();
+  const user = new SlytherinUser(); // Замените на нужный класс User
 
-    return [gryffindorUser, slytherinUser, ravenclawUser, hufflepuffUser];
+  return [slytherinBot, user];
 }
